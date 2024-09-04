c7b05db8bb7c4e885c3f36106f7e2e68d64212d3
Bruna Costa Lacerda Silva <brunasilva@brunas-air.speedport.ip>
2024-09-04 12:46:42 +0200

    Changes for app testing

--- app/user/user.model.js
+++ app/user/user.model.js
@@ -11,164 +11,165 @@ const SALT = "f844b09ff50c";
 
 let crypto;
 try {
-    crypto = await import("node:crypto");
+	crypto = await import("node:crypto");
 } catch (err) {
-    console.error("crypto support is disabled!", err);
+	console.error("crypto support is disabled!", err);
 }
 
 const userSchema = new Schema(
-    {
-        name: {
-            first: {
-                type: String,
-                required: "First name is required.",
-                minlength: [4, "First naame should have at least 4 letters."],
-                trim: true,
-            },
-            last: {
-                type: String,
-                required: "Last name is required.",
-                minlength: [4, "Last name should have at least 4 letters."],
-                trim: true,
-            },
-        },
-        email: {
-            type: String,
-            required: "E-mail is required.",
-            trim: true,
-            unique: true,
-            lowercase: true,
-            validate(email) {
-                if (!validator.isEmail(email)) {
-                    throw new Error("E-mail invalid! ");
-                }
-            },
-        },
+	{
+		name: {
+			first: {
+				type: String,
+				required: "First name is required.",
+				minlength: [4, "First naame should have at least 4 letters."],
+				trim: true,
+			},
+			last: {
+				type: String,
+				// required: "Last name is required.",
+				minlength: [4, "Last name should have at least 4 letters."],
+				trim: true,
+			},
+		},
+		email: {
+			type: String,
+			required: "E-mail is required.",
+			trim: true,
+			unique: true,
+			lowercase: true,
+			validate(email) {
+				if (!validator.isEmail(email)) {
+					throw new Error("E-mail invalid! ");
+				}
+			},
+		},
 
-        password: {
-            type: "Buffer",
-            required: "Password is required.",
-            minlength: [10, "Password is shorter than 10 characters."],
-            validate(value) {
-                if (!validator.isStrongPassword(value.toString())) {
-                    throw new Error(
-                        "Password is not strong enough. It should cointain at least: " +
-                            "1 letter lowercase, 1 letter uppercase, 1 number and 1 special character!"
-                    );
-                }
-            },
-        },
+		password: {
+			type: "Buffer",
+			required: "Password is required.",
+			minlength: [10, "Password is shorter than 10 characters."],
+			validate(value) {
+				if (!validator.isStrongPassword(value.toString())) {
+					throw new Error(
+						"Password is not strong enough. It should cointain at least: " +
+							"1 letter lowercase, 1 letter uppercase, 1 number and 1 special character!"
+					);
+				}
+			},
+		},
 
-        status: {
-            type: Number,
-            default: USER.STATUS.ACTIVE,
-            enum: [USER.STATUS, "Invalid status informed"],
-        },
+		status: {
+			type: Number,
+			default: USER.STATUS.ACTIVE,
+			enum: [USER.STATUS, "Invalid status informed"],
+		},
 
-        birthDate: {
-            type: Date,
-            validate(date) {
-                if (
-                    !validator.isDate(date, {
-                        format: DATE_FORMAT,
-                        delimiters: "/",
-                    })
-                ) {
-                    throw new Error("Birth date informed is invalid.");
-                }
-                const newDate = new Date();
-                const parsedDate = parse(date, DATE_FORMAT, newDate);
+		birthDate: {
+			type: Date,
+			validate(date) {
+				if (
+					!validator.isDate(date, {
+						format: DATE_FORMAT,
+						delimiters: "/",
+					})
+				) {
+					throw new Error("Birth date informed is invalid.");
+				}
+				const newDate = new Date();
+				// const parsedDate = parse(date, DATE_FORMAT, newDate);
 
-                if (differenceInYears(newDate, parsedDate) < 18) {
-                    throw new Error("User must be at least 18-year-old.");
-                }
-            },
-        },
-        skinType: {
-            type: String,
-            enum: SKIN_TYPE,
-        },
-        concern: [
-            {
-                type: String,
-                enum: SKIN_CONCERN,
-                required: true,
-            },
-        ],
-        gender: {
-            type: String,
-            enum: USER.GENDER,
-        },
-    },
-    {
-        virtuals: {
-            fullName: {
-                get() {
-                    return this.name.first + " " + this.name.last;
-                },
-            },
-            age: {
-                get() {
-                    return differenceInYears(new Date(), this.birthDate);
-                },
-            },
-        },
-        timestamps: true,
-    }
+				if (differenceInYears(newDate, date) < 18) {
+					throw new Error("User must be at least 18-year-old.");
+				}
+			},
+		},
+		skinType: {
+			type: String,
+			enum: SKIN_TYPE,
+		},
+		concern: [
+			{
+				type: String,
+				enum: SKIN_CONCERN,
+				required: true,
+			},
+		],
+		gender: {
+			type: String,
+			enum: USER.GENDER,
+		},
+		firstLogin: Date,
+	},
+	{
+		virtuals: {
+			fullName: {
+				get() {
+					return this.name.first + " " + this.name.last;
+				},
+			},
+			age: {
+				get() {
+					return differenceInYears(new Date(), this.birthDate);
+				},
+			},
+		},
+		timestamps: true,
+	}
 );
 
 userSchema.methods.toJSON = function () {
-    const user = this;
-    const userObject = user.toObject();
+	const user = this;
+	const userObject = user.toObject();
 
-    delete userObject.password;
+	delete userObject.password;
 
-    return userObject;
+	return userObject;
 };
 
 userSchema.statics.findByCredential = async (email, password) => {
-    const user = await User.findOne({ email });
-    if (!user) return;
-    const hash = crypto.pbkdf2Sync(password, SALT, 310000, 32, `sha256`);
+	const user = await User.findOne({ email });
+	if (!user) return;
+	const hash = crypto.pbkdf2Sync(password, SALT, 310000, 32, `sha256`);
 
-    if (!crypto.timingSafeEqual(user.password, hash)) {
-        throw new Error("Incorrect username or password.");
-    }
-    return user;
+	if (!crypto.timingSafeEqual(user.password, hash)) {
+		throw new Error("Incorrect username or password.");
+	}
+	return user;
 };
 
 userSchema.pre("save", async function (next) {
-    const user = this;
-    if (user.isModified("password")) {
-        user.password = crypto.pbkdf2Sync(
-            this.password,
-            SALT,
-            310000,
-            32,
-            `sha256`
-        );
-    }
+	const user = this;
+	if (user.isModified("password")) {
+		user.password = crypto.pbkdf2Sync(
+			this.password,
+			SALT,
+			310000,
+			32,
+			`sha256`
+		);
+	}
 
-    next();
+	next();
 });
 
 //Error handler middleware
 userSchema.post("save", function (error, doc, next) {
-    console.log("error 1");
-    if (error.name === "MongoServerError" && error.code === 11000) {
-        console.log(error);
-        return next(
-            Object.keys(error.keyValue)[0] + ": There was a duplicate key error"
-        );
-    }
-    if (error.name == "ValidationError") {
-        const arMessages = Object.keys(error.errors).map(
-            (key) => error.errors[key].message
-        );
-        return next(new Error(arMessages.join(" ")));
-    }
+	console.log("error 1");
+	if (error.name === "MongoServerError" && error.code === 11000) {
+		console.log(error);
+		return next(
+			Object.keys(error.keyValue)[0] + ": There was a duplicate key error"
+		);
+	}
+	if (error.name == "ValidationError") {
+		const arMessages = Object.keys(error.errors).map(
+			(key) => error.errors[key].message
+		);
+		return next(new Error(arMessages.join(" ")));
+	}
 
-    next(error);
+	next(error);
 });
 
 const User = mongoose.model("User", userSchema);
@@ -176,9 +177,9 @@ const User = mongoose.model("User", userSchema);
 export default User;
 
 (async function () {
-    try {
-        await User.ensureIndexes();
-    } catch (error) {
-        console.log(error);
-    }
+	try {
+		await User.ensureIndexes();
+	} catch (error) {
+		console.log(error);
+	}
 })();
--- app/user/user.router.js
+++ app/user/user.router.js
@@ -8,85 +8,96 @@ import auth from "../middleware/auth.js";
 import { HTTP_STATUS } from "../utils/enum.js";
 
 passport.use(
-    new LocalStrategy(
-        {
-            usernameField: "email",
-            passwordField: "password",
-        },
-        service.verify
-    )
+	new LocalStrategy(
+		{
+			usernameField: "email",
+			passwordField: "password",
+		},
+		service.verify
+	)
 );
 
 passport.serializeUser(function (user, cb) {
-    nextTick(function () {
-        cb(null, { id: user._id, email: user.email });
-    });
+	nextTick(function () {
+		cb(null, { id: user._id, email: user.email });
+	});
 });
 
 passport.deserializeUser(function (user, cb) {
-    nextTick(function () {
-        return cb(null, user);
-    });
+	nextTick(function () {
+		return cb(null, user);
+	});
 });
 
 const router = express.Router();
 
-router.post("/login", passport.authenticate("local", {}), (req, res) => {
-    res.send();
-});
+router.post(
+	"/login",
+	passport.authenticate("local", { failWithError: true }),
+	(req, res) => {
+		const resUser = {
+			name: req.user.name,
+			firstLogin: req.user.firstLogin,
+			isNew: true,
+		};
+		console.log(resUser);
+		res.send({
+			user: resUser,
+		});
+	}
+);
 
 router.post("/signup", async (req, res, next) => {
-    try {
-        if (!req.body) return res.status(HTTP_STATUS.BAD_REQUEST).send();
+	try {
+		if (!req.body) return res.status(HTTP_STATUS.BAD_REQUEST).send();
 
-        const user = await service.newUser(req.body);
-        res.status(HTTP_STATUS.CREATED).send(user);
-    } catch (error) {
-        console.log(error);
-        next({
-            message: error.message,
-            status: HTTP_STATUS.BAD_REQUEST,
-            cause: error,
-        });
-    }
+		const user = await service.newUser(req.body);
+		res.status(HTTP_STATUS.CREATED).send(user);
+	} catch (error) {
+		console.log(error);
+		next({
+			message: error.message,
+			status: HTTP_STATUS.BAD_REQUEST,
+			cause: error,
+		});
+	}
 });
 
 router.post("/logout", (req, res, next) => {
-    req.logout((err) => {
-        if (err) return next(err);
-        res.send();
-        //   res.redirect('/');
-    });
+	req.logout((err) => {
+		if (err) return next(err);
+		res.send();
+		//   res.redirect('/');
+	});
 });
 
 router.patch("/me", auth, async (req, res, next) => {
-    try {
-        if (!req.body) return res.status(HTTP_STATUS.BAD_REQUEST).send();
+	try {
+		if (!req.body) return res.status(HTTP_STATUS.BAD_REQUEST).send();
 
-        const user = await service.updateUser(req.body, req.user);
-        res.status(HTTP_STATUS.OK).send(user);
-    } catch (error) {
-        next({
-            message: error.message,
-            status: HTTP_STATUS.BAD_REQUEST,
-            cause: error,
-        });
-    }
+		const user = await service.updateUser(req.body, req.user);
+		res.status(HTTP_STATUS.OK).send(user);
+	} catch (error) {
+		next({
+			message: error.message,
+			status: HTTP_STATUS.BAD_REQUEST,
+			cause: error,
+		});
+	}
 });
 
 router.post("/survey", auth, async (req, res, next) => {
-    try {
-        if (!req.body) return res.status(HTTP_STATUS.BAD_REQUEST).send();
-
-        await service.newUserSurvey(req.body, req.user);
-        res.send();
-    } catch (error) {
-        next({
-            message: error.message,
-            status: HTTP_STATUS.BAD_REQUEST,
-            cause: error,
-        });
-    }
+	try {
+		if (!req.body) return res.status(HTTP_STATUS.BAD_REQUEST).send();
+		console.log(req.body);
+		res.send(await service.newUserSurvey(req.body, req.user));
+	} catch (error) {
+		next({
+			message: error.message,
+			status: HTTP_STATUS.BAD_REQUEST,
+			cause: error,
+		});
+	}
 });
 
 export default router;
